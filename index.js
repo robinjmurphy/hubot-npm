ce185b4a8ad6a68bf13720cde2d113af158a3e5e
Robin Murphy <robin.murphy@bbc.co.uk>
2016-06-01 22:25:50 +0100

    Add initial hook listener

--- index.js
+++ index.js
@@ -7,3 +7,35 @@
 // URLS:
 //   POST /hubot/npm?room=<room>
 'use strict';
+
+const getMessage = (data) => {
+  switch (data.event) {
+    case 'package:publish':
+      return `${data.name}@${data.version} published – https://www.npmjs.com/package/${data.name}`;
+  }
+};
+
+module.exports = (robot) => {
+  robot.router.post('/hubot/npm', (req, res) => {
+    const data = req.body;
+    let room = req.query.room;
+
+    if (!room) {
+      return res.status(400).end('Missing ?room parameter');
+    }
+
+    if (room && !/^#/.test(room)) {
+      room = `#${room}`;
+    }
+
+    const message = getMessage(data);
+
+    if (message) {
+      robot.send({
+        room: [room]
+      }, message);
+    }
+
+    res.end('OK');
+  });
+};
